8af4720ef8d29fa56a6b07d4295926c4e5611291
Richard Summer <richardforrestsummer@gmail.com>
2020-08-10 16:54:35 -0400

    Progress as of 8/10/20

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log("Hello")
+function openTab(tabName) {
+  var i;
+  var x = document.getElementsByClassName("tab");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById(tabName).style.display = "block";
+}
